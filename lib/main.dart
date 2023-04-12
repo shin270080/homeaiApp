fe0f98724ed6c256cbde944aca6c05f55a8d5189
shigematsukazuki <shin270080@gmail.com>
2023-04-12 17:57:03 +0900

    new game2

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,23 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(ColorfulNumberBlockTapGame());
+  runApp(ColorfulNumberRandomMovingBlockTapGame());
 }
 
-class ColorfulNumberBlockTapGame extends StatefulWidget {
+class ColorfulNumberRandomMovingBlockTapGame extends StatefulWidget {
   @override
-  _ColorfulNumberBlockTapGameState createState() =>
-      _ColorfulNumberBlockTapGameState();
+  _ColorfulNumberRandomMovingBlockTapGameState createState() =>
+      _ColorfulNumberRandomMovingBlockTapGameState();
 }
 
-class _ColorfulNumberBlockTapGameState
-    extends State<ColorfulNumberBlockTapGame> {
+class _ColorfulNumberRandomMovingBlockTapGameState
+    extends State<ColorfulNumberRandomMovingBlockTapGame> {
   int _score = 0;
   int _highScore = 0;
   int _seconds = 30;
   Timer? _timer;
+  List<_Block> _blocks = [];
   Random _random = Random();
-  int _number = 1;
-  Color _color = Colors.red;
 
   void _startTimer() {
     _timer = Timer.periodic(Duration(seconds: 1), (timer) {
@@ -44,39 +43,48 @@ class _ColorfulNumberBlockTapGameState
     setState(() {
       _score = 0;
       _seconds = 30;
-      _number = 1;
-      _color = Colors.red;
+      _blocks = [];
     });
   }
 
   void _startGame() {
     _stopGame();
     _startTimer();
-    _generateNumber();
+    _generateBlock();
   }
 
-  void _generateNumber() {
+  void _generateBlock() {
     setState(() {
-      _number = _random.nextInt(9) + 1;
-      _color = Color.fromRGBO(
-        _random.nextInt(256),
-        _random.nextInt(256),
-        _random.nextInt(256),
-        1.0,
-      );
+      _blocks.add(_Block(
+        number: _random.nextInt(9) + 1,
+        color: Color.fromRGBO(
+          _random.nextInt(256),
+          _random.nextInt(256),
+          _random.nextInt(256),
+          1.0,
+        ),
+        position: Offset(
+          _random.nextDouble() * MediaQuery.of(context).size.width,
+          _random.nextDouble() * MediaQuery.of(context).size.height,
+        ),
+        velocity: Offset(
+          (_random.nextDouble() - 0.5) * 10,
+          (_random.nextDouble() - 0.5) * 10,
+        ),
+      ));
     });
     Future.delayed(Duration(milliseconds: 1000), () {
       if (_seconds > 0) {
-        _generateNumber();
+        _generateBlock();
       }
     });
   }
 
-  void _onTap() {
-    if (_number == _score + 1) {
+  void _onTap(_Block block) {
+    if (block.number == _score + 1) {
       setState(() {
         _score++;
-        _number++;
+        _blocks.remove(block);
       });
     }
   }
@@ -84,45 +92,70 @@ class _ColorfulNumberBlockTapGameState
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Colorful Number Block Tap Game',
+      title: 'Colorful Number Random Moving Block Tap Game',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Colorful Number Block Tap Game'),
+          title: Text('Colorful Number Random Moving Block Tap Game'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Score: $_score'),
-              Text('High Score: $_highScore'),
-              Text('Time Left: $_seconds seconds'),
-              SizedBox(height: 50),
-              GestureDetector(
-                onTap: _onTap,
-                child: Container(
-                  width: 50,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: _color,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      '$_number',
-                      style: TextStyle(fontSize: 24),
+        body: Stack(
+          children: [
+            for (var block in _blocks)
+              Positioned(
+                left: block.position.dx,
+                top: block.position.dy,
+                child: GestureDetector(
+                  onTap: () => _onTap(block),
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: block.color,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        '${block.number}',
+                        style: TextStyle(fontSize: 24),
+                      ),
                     ),
                   ),
                 ),
               ),
-              SizedBox(height: 50),
-              ElevatedButton(
-                onPressed: _startGame,
-                child: Text('Start Game'),
+            Positioned(
+              bottom: 20,
+              left: 20,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Score: $_score'),
+                  Text('High Score: $_highScore'),
+                  Text('Time Left: $_seconds seconds'),
+
+                  ElevatedButton(
+                    onPressed: () => _startGame(),
+                    child: Text('Start Game'),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
+
   }
+}
+
+class _Block {
+  final int number;
+  final Color color;
+  Offset position;
+  Offset velocity;
+
+  _Block({
+    required this.number,
+    required this.color,
+    required this.position,
+    required this.velocity,
+  });
 }
